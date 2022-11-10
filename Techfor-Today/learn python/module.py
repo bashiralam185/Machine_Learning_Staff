a4a56577d0ec51409244401a12ad9376373bf8e1
bashiralam185 <bashiralam185@gmail.com>
2022-11-10 21:03:11 +0600

    added

--- Techfor-Today/learn python/module.py
+++ Techfor-Today/learn python/module.py
@@ -8,33 +8,34 @@
 # Import a library
 import random
 
-__all__ = ['__COLORS__', '__NAMES__', 'Person', 'random_person', 'random','__get_random_name__']
+# creating python __all__ variable
+__all__ = ['__MODEL__', '__CARS__', 'Car', 'random_car', 'random','get_random_name']
 
 # Define constants
-__COLORS__ = ("red", "blue", "yellow", "green")
-__NAMES__ = ("jake", "alam", "DK")
+__MODEL__ = (2014, 2016, 2018, 2020, 2022)
+__CARS__ = ("BMW", "Toyota")
 
 
 # Define a custom class
-class Person:
+class Car:
 
     #init method
-    def __init__(self, color: str, name: str):
-        self.color = color
+    def __init__(self, model: str, name: str):
+        self.model = model
         self.name = name
         
     def __str__(self):
-        return f"Name: {self.name},  Fav color is:  {self.color}"
+        return f"Name: {self.name},  Model:  {self.model}"
     
     
 # Define a custom function
-def __get_random_name__():
+def get_random_name():
     
     # returning random names and colors
-    return Person(random.choice(__COLORS__), random.choice(__NAMES__))
+    return Car(random.choice(__MODEL__), random.choice(__CARS__))
 
 # Create a random object
-random_person = get_random_name()
+random_car = get_random_name()
 
 
 # In[ ]:
---
+++ Techfor-Today/learn python/python_all.py
@@ -0,0 +1,8 @@
+# importing the module
+from module import *
+
+# Create new object 
+car = Car(__MODEL__[0], __CARS__[1])
+
+# printing
+print(car)
